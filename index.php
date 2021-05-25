7487b430b3aab25724a6752965ed03acf19d53b8
Shane Hastings <hastingsshane35@gmail.com>
2021-05-25 14:48:02 +0100

    Placeholder landing page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="https://cdn.shanehastings.eu/whereami/pin.png" />
+    <title>Where am I | Shane Hastings</title>
+    <meta name="description" content="A near real time geolocation travel map of my travels.">
+    <meta name="author" content="Shane Hastings">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link href="/css/default.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container-placeholder">
+    <div class="row">
+        <div class="card" style="padding: 25px;">
+
+            <a href="https://github.com/ShaneHastings/WhereAmI">
+            <img src="https://cdn.shanehastings.eu/whereami/pin.png">
+            </a>
+
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
+
